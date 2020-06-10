c81f92bd5354962f2496b6b75aab82d17eef99d2
kamilkowalczyk27 <kamilkowalczyk27@o2.pl>
2020-06-10 18:04:47 +0200

    New Frontend

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://infinite-falls-96094.herokuapp.com/v1/task/';
+  const apiRoot = 'https://infinite-falls-96094.herokuapp.com/v1/trello/';
   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
